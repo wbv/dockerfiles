11bd3aa25933900f76e54f559fca57ae8d5f790d
Walter B. Vaughan <wbv@users.noreply.github.com>
2023-02-04 13:18:59 -0500

    test GH action for auto-publish

--- build.sh
+++ build.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 
+# build.sh - build and tag docker images
+#
 # Usage:
-#   ./build.sh DOCKERFILE1 [DOCKERFILE2 [...]]
+#   ./build.sh IMAGE [IMAGE [...]]
+#
+# will build and tag each IMAGE from IMAGE.Dockerfile
+#
+# Environment Variables:
+#   IMAGE_PREFIX     an optional prefix to the image tag (omit the trailing '/')
 
-[ "$REPO_OWNER" != "" ] || REPO_OWNER="wbvn"
+
+# return value of the script, nonzero if anything failed
+SUCCESS=0
+
+# add a separator (trailing '/') for the image prefix if supplied
+if [ "${IMAGE_PREXIX}" != "" ]; then
+	IMAGE_PREFIX="${IMAGE_PREFIX}/"
+fi
 
 while [ $# -gt 0 ]; do
-	DOCKERFILE="$1"
+	DOCKERFILE="${1}".Dockerfile
 	if [ ! -r "$DOCKERFILE" ]; then
-		echo "'$DOCKERFILE' not found." >&2
-		DOCKERFILE="$1".Dockerfile
-		echo "Trying '$DOCKERFILE'." >&2
-		if [ ! -r "$DOCKERFILE" ]; then
-			echo "'$DOCKERFILE' not found. Skipping." >&2
-			shift
-			continue
-		fi
+		echo "'$DOCKERFILE' not found. Skipping." >&2
+		SUCCESS=1
+		shift
+		continue
 	fi
-	echo "found: $DOCKERFILE"
+	echo "building: $DOCKERFILE"
 
-	TAG="$REPO_OWNER/$(basename $DOCKERFILE .Dockerfile)"
-	echo "using tag: $TAG"
+	TAG="${IMAGE_PREFIX}${1}"
+	echo "with tag: $TAG"
 	shift
 	docker build \
 		-t "$TAG":latest \
